12b2e31c390e1f3aa32217ea016960c8247a6eab
lapin659 <67121911+lapin659@users.noreply.github.com>
2022-03-07 12:35:39 -0800

    Initialization of project

---
+++ src/main/java/com/webapp/paw2paw/Paw2PawApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.paw2paw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Paw2PawApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Paw2PawApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webapp/paw2paw/Paw2PawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.paw2paw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Paw2PawApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
