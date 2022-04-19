39257faaa85f4ad37173b3214a7fe741c019756f
lapin659 <67121911+lapin659@users.noreply.github.com>
2022-04-18 17:30:48 -0700

    closes #8.

--- src/main/java/com/webapp/paw2paw/controllers/productListController.java
+++ src/main/java/com/webapp/paw2paw/controllers/productListController.java
@@ -1,27 +1,37 @@
 package com.webapp.paw2paw.controllers;
 
+import com.webapp.paw2paw.model.Product;
 import com.webapp.paw2paw.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
 
 @Controller
 
 public class productListController {
+    /**
     @RequestMapping({ "Trading", "Trading/productList","Trading/productList.html"})
-   public String listProducts(){
+    public String listProducts(){
         return "Trading/productList";
     }
+     **/
 
+    @Autowired  private ProductService productService;
 
-private final ProductService productService;
+    @GetMapping("/productList.html")
+    public String listProducts(Model model){
+        List<Product> listProducts = productService.getAllProducts();
+        model.addAttribute("products", listProducts);
+        return "productList";
+    }
 
-    @Autowired
-    public productListController(ProductService productService) {
+    /**  public productListController(ProductService productService) {
         this.productService = productService;
     }
-/**
+
     @GetMapping ({"/Trading"})
     public String getAllProducts(Model model){
         List<Product> listProducts = productService.getAllProducts();
@@ -29,15 +39,9 @@ private final ProductService productService;
         return "Trading";
 
 } **/
-/**
-    @GetMapping("/Trading")
-    public String listProducts(Model model){
-        List<Product> listProducts = service.listAll();
-        model.addAttribute("listProducts", listProducts);
-        return "Trading";
-    }
 
-**/
+
+
 
 
 
