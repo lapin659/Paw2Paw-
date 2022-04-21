bb45c6d05749e74b4fb5f68eb6b1b19c64c1adcf
Liang Zhang <liangz@bu.edu>
2022-04-21 07:19:06 -0700

    DB connection established. Test completed and passed. Closes #13. Closes #14

---
+++ src/test/java/com/webapp/paw2paw/UserRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.webapp.paw2paw;
+
+import com.webapp.paw2paw.model.User;
+import com.webapp.paw2paw.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+import static org.assertj.core.api.Assertions.assertThat;
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(value = false)
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository repo;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Test
+    public void testCreatUser(){
+        User user = new User();
+        user.setEmail("123@gmail.com");
+        user.setUsername("Lulu");
+        user.setPassword("aabb");
+
+        User savedUser = repo.save(user);
+        User existUser = entityManager.find(User.class, savedUser.getId());
+        assertThat(existUser.getEmail()).isEqualTo(user.getEmail());
+
+    }
+
+}
