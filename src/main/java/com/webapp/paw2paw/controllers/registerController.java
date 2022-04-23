955b0df899164334b8ff001302eb0eafc8598ebd
Liang Zhang <liangz@bu.edu>
2022-04-22 21:25:18 -0700

    Closes #15

---
+++ src/main/java/com/webapp/paw2paw/controllers/registerController.java
@@ -0,0 +1,26 @@
+package com.webapp.paw2paw.controllers;
+
+import com.webapp.paw2paw.model.User;
+import com.webapp.paw2paw.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class registerController {
+    @Autowired
+    private UserRepository repo;
+
+    @GetMapping("/register.html")
+    public String showRegister(Model model){
+        model.addAttribute("user", new User());
+        return "register";
+    }
+    @PostMapping("/save_user")
+    public String saveUser(User user){
+        repo.save(user);
+        return "user_saved";
+    }
+}
