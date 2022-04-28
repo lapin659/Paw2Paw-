d6b02ab8312ffa73df75928ca55785d718274f87
Liang Zhang <liangz@bu.edu>
2022-04-27 21:29:35 -0700

    redirect to exchange form. Closes #21

--- src/main/java/com/webapp/paw2paw/controllers/exchangeProductController.java
+++ src/main/java/com/webapp/paw2paw/controllers/exchangeProductController.java
@@ -1,33 +1,24 @@
 package com.webapp.paw2paw.controllers;
 
-import com.webapp.paw2paw.model.Product;
 import com.webapp.paw2paw.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 
-public class productListController {
+public class exchangeProductController {
 
     @Autowired  private ProductService productService;
 
-    @GetMapping("/productList.html")
-    public String listProducts(Model model){
-        List<Product> listProducts = productService.getAllProducts();
-        model.addAttribute("products", listProducts);
-        return "productList";
-    }
-/**
-    @GetMapping("/{productId}")
+    @GetMapping("/exchange/{productId}")
     public String exchangeProduct(Model model, @PathVariable("productId") String productId){
        model.addAttribute("exchange", productService.getProductById(productId));
         return "exchange";
     }
-**/
+
 
 
 
