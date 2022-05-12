433379e0069abdf4c02dd77b741a34a7c7f25370
Liang Zhang <liangz@bu.edu>
2022-05-11 21:55:24 -0700

    Closes #27

--- src/main/java/com/webapp/paw2paw/controllers/userProfileController.java
+++ src/main/java/com/webapp/paw2paw/controllers/userProfileController.java
@@ -1,6 +1,10 @@
 package com.webapp.paw2paw.controllers;
 
 import com.webapp.paw2paw.model.OrderHistory;
+import com.webapp.paw2paw.model.User;
+import com.webapp.paw2paw.repository.UserRepository;
+import com.webapp.paw2paw.service.UserProfileService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +15,13 @@ import java.security.Principal;
 
 @Controller
 public class userProfileController {
+    private UserProfileService userProfileService;
+    @Autowired
+    private UserRepository userRepos;
 
     @GetMapping("/login")
     public String showLogin(){
+
         return "login";
 
     }
@@ -22,9 +30,12 @@ public class userProfileController {
     @GetMapping(value = {"/","/user_profile"})
     //@ResponseBody
     public String userProfile(Principal principal, Model model){
+
         String currUser = principal.getName();
         model.addAttribute("currUsername", currUser);
-
+        User curr = userRepos.findByEmail(currUser);
+       // String curr = userRepos.findByEmail(currUser).getUsername();
+        model.addAttribute("currUser", curr);
         return "user_profile";
 
     }
--- src/main/java/com/webapp/paw2paw/repository/UserRepository.java
+++ src/main/java/com/webapp/paw2paw/repository/UserRepository.java
@@ -4,9 +4,12 @@ package com.webapp.paw2paw.repository;
 import com.webapp.paw2paw.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query("SELECT u FROM User u WHERE u.email = ?1")
     User findByEmail(String email);
+    User findByUsername(String username);
 
 }
