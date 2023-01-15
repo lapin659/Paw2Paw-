0a4bd88b94aa6ae186d898c4225ca907d1ba83aa
Liang Zhang <liangz@bu.edu>
2023-01-15 09:16:41 -0800

    Table added, closes #34

--- src/main/java/com/webapp/paw2paw/controllers/forumController.java
+++ src/main/java/com/webapp/paw2paw/controllers/forumController.java
@@ -3,6 +3,7 @@ package com.webapp.paw2paw.controllers;
 
 import com.webapp.paw2paw.model.ForumTopic;
 import com.webapp.paw2paw.repository.TopicRepository;
+import com.webapp.paw2paw.service.TopicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,16 +19,30 @@ public class forumController {
     public forumController(TopicRepository topicRepository) {
         this.topicRepository = topicRepository;
     }
- @RequestMapping({"forum.html"})
+    @Autowired
+    TopicService topicService;
+    @RequestMapping({"forum.html"})
     public String displayForum(Model model){
         return "forum";
     }
-
+/**
 @GetMapping("forum")
 public String displayTopics(Model m){
     List<ForumTopic> topics = topicRepository.findAll();
     m.addAttribute("topics", topics);
     return "forum";
 }
+ **/
+    @GetMapping("forum")
+    public String listTopics(Model model){
+        List<ForumTopic> listTopics = topicService.getAllTopics();
+        model.addAttribute("listedtopics", listTopics);
+        return "forum";
+    }
+
+
+
+
+
 
 }
--- src/main/java/com/webapp/paw2paw/service/TopicService.java
+++ src/main/java/com/webapp/paw2paw/service/TopicService.java
@@ -14,9 +14,11 @@ public class TopicService {
         List<ForumTopic> listOfTopics = new ArrayList<>();
         long User1 = 1000L;
         long User2 = 2000L;
+        User USER1 = new User();
+        User USER2 = new User();
 
-        listOfTopics.add(new ForumTopic(User1, "UserA",  new User()));  //construct user or call usertitle?
-        listOfTopics.add(new ForumTopic(User2, "UserB", new User()));
+        listOfTopics.add(new ForumTopic(User1, "UserA", USER1));  //construct user or call usertitle?
+        listOfTopics.add(new ForumTopic(User2, "UserB", USER2));
 
         return listOfTopics;
     }
